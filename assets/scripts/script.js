85bcb37f1df8a90159351d7ae27a616266fea1cf
s4urp8n <s4urp8n@yandex.ru>
2017-12-03 01:15:12 +0300

    initial

---
+++ assets/scripts/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $(function () {
+
+        var slidesRoot = '/assets/images/slides/';
+
+        $('body').vegas({
+            slides: [
+                {src: slidesRoot + '2.jpg'},
+                {src: slidesRoot + '6.jpg'},
+                {src: slidesRoot + '7.jpg'}
+            ],
+            overlay: true,
+            shuffle: false,
+            delay: 15000
+        });
+    });
+
+//------------------------------
+//ONLY FOR DEMO
+//------------------------------
+    $('.header-menu-item').on('click', function (event) {
+
+        $('.header-menu-item').removeClass('header-menu-item--active');
+        $(this).addClass('header-menu-item--active');
+
+        if ($(this).attr('href') == '#') {
+            $('.page').removeClass('page--home');
+        } else {
+            $('.page').addClass('page--home');
+        }
+
+        event.preventDefault();
+    });
+});
