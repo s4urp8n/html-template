f5a4143cbd5dede3d8da0afa315c083bb097a015
s4urp8n <s4urp8n@yandex.ru>
2017-12-09 17:20:46 +0300

    initial

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 
+    $(".photobox_container").photobox("a", {
+        time: 1000,
+        autoplay: false,
+        history: true,
+        loop: true,
+        thumbs: true,
+        hideFlash: true,
+        counter: true
+    });
+
+    setTimeout(window._photobox.history.load, 100);
+
 });
